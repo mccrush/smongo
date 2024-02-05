a66052f11221aebbee273451eb625c85c98cf395
mccrush <mccrush2027@gmail.com>
2024-02-05 22:53:23 +0700

    Remove URL

--- db.js
+++ db.js
@@ -1,8 +1,5 @@
 const { MongoClient } = require('mongodb')
-
-
-
-//const URL = 'mongodb://c86003_nomo_na4u_ru:LaRgeCijpusad37@mongo1.c86003.h2,mongo2.c86003.h2,mongo3.c86003.h2/c86003_nomo_na4u_ru?replicaSet=MongoReplica'
+//const { URL } = require('vars.js')
 
 const URL = 'mongodb://127.0.0.1:27017/'
 
