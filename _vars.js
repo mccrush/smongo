512398b1599c6b8e72efef9b7e00218e04a7ca32
mccrush <mccrush2027@gmail.com>
2024-02-09 20:44:04 +0700

    Settings Envs

---
+++ _vars.js
@@ -0,0 +1,1 @@
+export const DB_URL = 'mongodb://c86003_nomo_na4u_ru:Q6DhHfNCu@mongo1.c86003.h2,mongo2.c86003.h2,mongo3.c86003.h2/c86003_nomo_na4u_ru_erp?replicaSet=MongoReplica'
--- server.js
+++ server.js
@@ -1,34 +1,53 @@
-//require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
-const { connectToDb, getDb } = require('./db')
+const mongoose = require('mongoose')
+const { ObjectId } = require('mongodb')
+//const { connectToDb, getDb } = require('./db')
 
-//const APP_ADDRESS = '127.0.5.66:55542'
-//const APP_ADDRESS = process.env.APP_ADDRESS
-//console.log(`server.js APP_ADDRESS = ${APP_ADDRESS}`);
-const APP_ADDRESS = 27017
+const APP_ADDRESS = process.env.APP_ADDRESS_D
+const DB_URL = process.env.DB_URL_D
 
 const app = express()
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
+const start = async () => {
+  try {
+    await mongoose.connect(DB_URL)
+    console.log(`server.js start() DB is connect`)
+  } catch (error) {
+    console.log(`server.js start() Error connect to DB ${error}`)
+  }
+}
+
+start()
+
+app.listen(APP_ADDRESS, (error) => {
+  error ? console.log(error) : console.log(`server.js Listening port: ${APP_ADDRESS}`)
+})
+
 
 let db
 
-connectToDb(error => {
-  if (!error) {
-    app.listen(APP_ADDRESS, (error) => {
-      error ? console.log(error) : console.log(`server.js Listening port: ${APP_ADDRESS}`)
+// connectToDb(error => {
+//   if (!error) {
+//     app.listen(APP_ADDRESS, (error) => {
+//       error ? console.log(error) : console.log(`server.js Listening port: ${APP_ADDRESS}`)
 
-    })
-    db = getDb()
-  } else {
-    console.log(`server.js DB connection error ${error}`);
-  }
-})
+//     })
+//     db = getDb()
+//   } else {
+//     console.log(`server.js DB connection error ${error}`);
+//   }
+// })
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 
 app.get('/companies', (req, res) => {
   const companies = []
@@ -66,5 +85,69 @@ app.get('/users', (req, res) => {
         .status(500)
         .json({ error: "Ошибка при получении данных users с сервера" })
     })
+})
+
+app.get('/users/:id', (req, res) => {
+  db
+    .collection('users')
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then((doc) => {
+      res
+        .status(200)
+        .json(doc)
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Ошибка при получении данных users с сервера" })
+    })
+})
+
+
+app.delete('/users/:id', (req, res) => {
+  db
+    .collection('users')
+    .deleteOne({ _id: new ObjectId(req.params.id) })
+    .then((result) => {
+      res
+        .status(200)
+        .json(result)
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Ошибка при удалении данных users с сервера" })
+    })
+})
+
+
+app.post('/users', async (req, res) => {
+  try {
+    const result = await db.collection('users').insertOne(req.body)
+    res
+      .status(201)
+      .json(result)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Ошибка при добавлении данных users в БД" })
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    try {
+      const result = await db.collection('users').updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+      res
+        .status(201)
+        .json(result)
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Ошибка при обновлении данных users в БД" })
+    }
+  } else {
+    console.log(`server.js при обновлении данных указан не корректный Id`);
+  }
 })
 
