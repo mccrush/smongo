c8193b46fd52ab12fdb97a73d864bc2db170aa97
mccrush <mccrush2027@gmail.com>
2024-02-11 15:15:31 +0700

    Update listeners addreses

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const mongoose = require('mongoose')
 const userRoutes = require('./routes/user-routes')
 
 
-const APP_ADDRESS = process.env.APP_ADDRESS_D
+const APP_IP = process.env.APP_IP_D
+const APP_PORT = process.env.APP_PORT_D
 const DB_URL = process.env.DB_URL_D
+
+// const APP_IP = process.env.APP_IP
+// const APP_PORT = process.env.APP_PORT
+// const DB_URL = process.env.DB_URL
 
 const app = express()
 app.use(express.json())
@@ -22,9 +27,12 @@ const start = async () => {
   try {
     await mongoose.connect(DB_URL)
     console.log(`server.js start() DB is connect`)
-    app.listen(APP_ADDRESS, (error) => {
-      error ? console.log(error) : console.log(`server.js Listening port: ${APP_ADDRESS}`)
+    app.listen(APP_PORT, (error) => {
+      error ? console.log(error) : console.log(`server.js Listening port: ${APP_PORT}`)
     })
+    // app.listen(APP_PORT, APP_IP, (error) => {
+    //   error ? console.log(error) : console.log(`server.js Listening port: ${APP_PORT}`)
+    // })
   } catch (error) {
     console.log(`server.js start() Error connect to DB ${error}`)
   }
