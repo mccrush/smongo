6885598db22f254f8b43c8765bcf35316b8a36ac
mccrush <mccrush2027@gmail.com>
2024-02-13 11:57:19 +0700

    Add condition

--- server.js
+++ server.js
@@ -3,21 +3,28 @@ const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user-routes')
 
+let APP_IP = ''
+let APP_PORT = ''
+let DB_URL = ''
 
-// const APP_IP = process.env.APP_IP_D
-// const APP_PORT = process.env.APP_PORT_D
-// const DB_URL = process.env.DB_URL_D
+if (process.env.APP_ENV === 'dev') {
+  APP_IP = process.env.APP_IP_D
+  APP_PORT = process.env.APP_PORT_D
+  DB_URL = process.env.DB_URL_D
+} else if (process.env.APP_ENV === 'prod') {
+  APP_IP = process.env.APP_IP
+  APP_PORT = process.env.APP_PORT
+  DB_URL = process.env.DB_URL
+}
 
-const APP_IP = process.env.APP_IP
-const APP_PORT = process.env.APP_PORT
-const DB_URL = process.env.DB_URL
 
 const app = express()
 app.use(express.json())
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://testapp.na4u.ru')
+  //res.setHeader('Access-Control-Allow-Origin', 'https://testapp.na4u.ru')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
   res.setHeader('Access-Control-Allow-Credentials', true)
@@ -30,12 +37,15 @@ const start = async () => {
   try {
     await mongoose.connect(DB_URL)
     console.log(`server.js start() DB is connect`)
-    // app.listen(APP_PORT, (error) => {
-    //   error ? console.log(error) : console.log(`server.js Listening port: ${APP_PORT}`)
-    // })
-    app.listen(APP_PORT, APP_IP, (error) => {
-      error ? console.log(error) : console.log(`server.js Listening port: ${APP_PORT}`)
-    })
+    if (process.env.APP_ENV === 'dev') {
+      app.listen(APP_PORT, (error) => {
+        error ? console.log(error) : console.log(`server.js Listening dev port: ${APP_PORT}`)
+      })
+    } else if (process.env.APP_ENV === 'prod') {
+      app.listen(APP_PORT, APP_IP, (error) => {
+        error ? console.log(error) : console.log(`server.js Listening prod addres: ${APP_PORT}:${APP_PORT}`)
+      })
+    }
   } catch (error) {
     console.log(`server.js start() Error connect to DB ${error}`)
   }
