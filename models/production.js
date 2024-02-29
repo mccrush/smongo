2347f9b4aee6d8a4f741d534840574a7dc387edf
mccrush <mccrush2027@gmail.com>
2024-02-29 17:26:36 +0700

    Update properti name

--- models/production.js
+++ models/production.js
@@ -5,7 +5,7 @@ const productionSchema = new Schema({
   title: { type: String, required: true },
   arhive: { type: String, default: false },
   stageId: { type: String, required: true },
-  manager: { type: String, required: true },
+  managerId: { type: String, required: true },
   description: { type: String },
 })
 
